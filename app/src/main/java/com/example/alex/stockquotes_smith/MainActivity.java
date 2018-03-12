7cd66b14dcac3127131e3100fcb853106cf6ba19
Smitha6 <smitha6@g.cofc.edu>
2018-03-12 09:36:11 -0400

    2nd Commit

--- app/src/main/java/com/example/alex/stockquotes_smith/MainActivity.java
+++ app/src/main/java/com/example/alex/stockquotes_smith/MainActivity.java
@@ -2,12 +2,54 @@ package com.example.alex.stockquotes_smith;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
+
+    private TextView displaySymbol;
+    private TextView displayName;
+    private TextView displayLastTradePrice;
+
+
+
+
+    private Stock stock;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        displaySymbol = findViewById(R.id.displaySymbol);
+        displayName = findViewById(R.id.displayName);
+        displayLastTradePrice = findViewById(R.id.displayLastTradePrice);
+
+        //gets symbol
+        editText = findViewById(R.id.editText);
+
+        //create stock object with new symbol
+        //stock = new Stock(editText.toString());
+
+        stock = new Stock("goog");
+        try {
+            stock.load();
+
+        }
+        catch (Exception e)
+        {
+            System.out.println(e);
+        }
+
+        displaySymbol.setText(stock.getSymbol().toString());
+        //displayName.setText(stock.getName().toString());
+        //displayLastTradePrice.setText(stock.getLastTradePrice().toString());
+
+
+
+
     }
 }
